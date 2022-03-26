623966e8659357a1e0805e948209d6c29e0b6217
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-03-26 23:08:31 +0700

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React, { useEffect } from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=92696fa0';
 
 const App = () => {
+
+  const searchMovie = async (title) => {
+    //call api
+    const response = await fetch(`${API_URL}&s=${title}`)
+    //after get response we have to get data from json
+    const data = await response.json();
+
+    console.log(data.Search)
+  }
+
+  useEffect(()=>{
+    searchMovie('Spiderman')
+  },[])
   
   return (
     <div className="App">
