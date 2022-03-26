0b3eeb9ba60262840bfc96593b13d5cbf8da27b0
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-03-27 01:04:33 +0700

    Add search movie state and Wrap up project

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ import MovieCard from "./MovieCard";
 
 const API_URL = "http://www.omdbapi.com?apikey=92696fa0";
 
-const movie1 = {
-  Title: "Spiderman and Grandma",
-  Year: "2009",
-  imdbID: "tt1433184",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('');
 
   const searchMovie = async (title) => {
     //call api
@@ -27,7 +19,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovie("Spiderman");
+    searchMovie("captain");
   }, []);
 
   return (
@@ -37,10 +29,10 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value) }
         ></input>
-        <img src={SearchIcon} alt="search" />
+        <img src={SearchIcon} alt="search" onClick={()=> searchMovie(searchTerm)}/>
       </div>
       {movies?.length > 0 ? (
         <div className="container">
